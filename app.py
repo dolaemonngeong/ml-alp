7131380590f0230738a6fac33606e46213f4c67a
Davina T W <davinawijaya03@gmail.com>
2023-12-31 13:46:33 +0700

    using pickle and adding app.py

---
+++ app.py
@@ -0,0 +1,46 @@
+from flask import Flask, render_template, Response, request, redirect, url_for
+import subprocess
+import pickle
+import numpy as np
+from gevent.pywsgi import WSGIServer
+
+# Load ML model
+model = pickle.load(open('model.pkl', 'rb')) 
+
+# Create application
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/predict', methods =['POST'])
+def predict():
+    
+    # Put all form entries values in a list 
+    features = [float(i) for i in request.form.values()]
+    # bp_category = 
+    # features.append()
+
+    # Convert features to array
+    array_features = [np.array(features)]
+
+    # Predict features
+    prediction = model.predict(array_features)
+    
+    output = prediction
+    
+
+    # Check the output values and retrive the result with html tag based on the value
+    if output == 1:
+        return render_template('index.html', 
+                               result = 'You have no sleeping disorders.')
+    elif output == 2:
+        return render_template('index.html', 
+                               result = 'You have Sleep Apnea')
+    elif output == 3:
+        return render_template('index.html', 
+                               result = 'You have Insomnia')
+
+app.run(debug=True)
+    
---
+++ predictioin.py
@@ -0,0 +1,4 @@
+import pandas as pd
+import pickle
+
+df = pd.read_csv("Sleep_health_and_lifestyle_dataset.csv")
